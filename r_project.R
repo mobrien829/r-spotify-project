269534e57861891826f64cd8cb77ebd9f12e9db7
Michael <mlobrien829@gmail.com>
2022-05-06 16:32:58 -0400

    created a locally cached csv file, added csvs to the git ignore

--- r_project.R
+++ r_project.R
@@ -8,3 +8,13 @@ test_df = get_featured_playlists()
 
 test_playlist = get_playlist("37i9dQZF1DWXJfnUiYjUKT")
 audio_features_test = get_playlist_audio_features(playlist_uris = c(test_playlist$id))
+
+# need to fetch all playlists by ids
+
+playlist_df = get_playlist_audio_features(playlist_uris = c(test_df$id))
+
+# select columns that will be most useful
+playlist_df = playlist_df %>% select(playlist_id, danceability, energy, key, loudness, mode, speechiness, acousticness, valence, tempo, track.id, track.duration_ms, track.name, track.album.name)
+
+# WRITE CSV to locally cache data
+# write.csv(playlist_df, "./playlist_csv_20220506.csv", row.names = FALSE)
