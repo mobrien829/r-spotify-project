44920c017f112241a5612bbf2cf9fa61f69913dc
Michael <mlobrien829@gmail.com>
2022-05-06 15:34:27 -0400

    managed to isolate one playlist

--- r_project.R
+++ r_project.R
@@ -3,3 +3,8 @@ library(tidyverse)
 
 token = get_spotify_access_token()
 
+
+test_df = get_featured_playlists()
+
+test_playlist = get_playlist("37i9dQZF1DWXJfnUiYjUKT")
+audio_features_test = get_playlist_audio_features(playlist_uris = c(test_playlist$id))
