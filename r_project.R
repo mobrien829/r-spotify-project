4ffdeaae67045399dc0f4247427186f25c0af4d9
Michael <mlobrien829@gmail.com>
2022-05-08 23:06:54 -0400

    popularity against energy plot

--- r_project.R
+++ r_project.R
@@ -14,10 +14,36 @@ audio_features_test = get_playlist_audio_features(playlist_uris = c(test_playlis
 playlist_df = get_playlist_audio_features(playlist_uris = c(test_df$id))
 
 # select columns that will be most useful
-playlist_df = playlist_df %>% select(playlist_id, danceability, energy, key, loudness, mode, speechiness, acousticness, valence, tempo, track.id, track.duration_ms, track.name, track.album.name)
+playlist_df = playlist_df %>% select(playlist_id, danceability, energy, key, loudness, mode, speechiness, acousticness, valence, tempo, track.id, track.artists, track.popularity, track.duration_ms, track.name, track.album.name)
 
 # WRITE CSV to locally cache data
-# write.csv(playlist_df, "./playlist_csv_20220506.csv", row.names = FALSE)
+# write.csv(playlist_df, "./playlist_csv_2022058.csv", row.names = FALSE)
 
 # read_csv to read the cached file
 # playlist_df = read.csv("playlist_csv_20220506.csv")
+
+playlist_df = playlist_df %>% unnest(track.artists)
+
+# artist_df = get_artists(ids = c(playlist_df$id))
+
+new_playlist_df = playlist_df %>% group_by(playlist_name)
+table(playlist_df$track.name)
+summary(playlist_df)
+# sample random 50 from the playlist
+playlist_sample = playlist_df %>% sample_n(50)
+artists_df = get_artists(ids = c(playlist_sample$id))
+
+
+
+artists_df %>% ggplot(aes(x = popularity, y = log(followers.total))) + geom_point() + labs(x="Popularity", y="Log of followers", title="Popularity vs. Log of Followers of 50 Artists")
+
+
+?inner_join
+playlist_sample = playlist_sample %>% inner_join(artists_df, by = c("name", "uri", "id", "type", "external_urls.spotify", "href" ))
+
+playlist_df %>% ggplot(aes(track.popularity)) + geom_density() + theme(axis.ticks.y=element_blank(), axis.text.y = element_blank()) + labs(x="Popularity", y="Density", title="Density of Popularity")
+
+playlist_sample %>% 
+  ggplot(aes(x = energy, y = track.popularity)) + geom_point(aes(color=tempo)) + labs(x="Energy", y="Popularity", title="Popularity against energy")
+
+
